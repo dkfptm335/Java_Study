591f2f9bf56accde125a454f9bdb8f2d38f3f2df
Gloomy <dkfptm335@naver.com>
2024-03-26 17:31:05 +0900

    Initial Commit

---
+++ java-start/src/CommentJava.java
@@ -0,0 +1,14 @@
+public class CommentJava {
+
+    /*주석을 설명하는 부분입니다.*/
+    public static void main(String[] args){
+        System.out.println("Hello Java1"); // hello java1을 출력합니다.
+
+        //System.out.println("Hello Java1");
+
+        /* 여러 줄 주석
+        System.out.println("Hello Java1");
+        System.out.println("Hello Java1");
+        */
+    }
+}
